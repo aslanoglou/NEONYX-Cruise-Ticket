46404c8baa515471a524d2367b2c003e0a27639e
Jordan <i.aslanoglou@gazzetta.gr>
2024-04-19 23:17:23 +0300

    header / section1

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,12 @@ const path = require('path')
 const autoprefixer = require('autoprefixer')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const miniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyPlugin = require("copy-webpack-plugin");
 module.exports = {
     mode: 'development',
     // entry: './src/js/main.js',
     entry: {
-        // main: './src/js/main.js',
+        main: './src/js/main.js',
         // critical: '/src/critical.js',
         // bootstrap: '/src/bootstrap.js',
     },
@@ -20,7 +21,19 @@ module.exports = {
     devServer: {
         static: path.resolve(__dirname, 'dist'), port: 8080, hot: true
     },
-    plugins: [new HtmlWebpackPlugin({template: './src/index.html'}), new miniCssExtractPlugin()],
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',  // Output filename for index.html
+        }),
+        new miniCssExtractPlugin(),
+        new CopyPlugin({
+            patterns: [
+                { from: "src/images", to: "images" },
+                // { from: "src/fonts", to: "fonts" },
+            ],
+        }),
+    ],
     module: {
         rules: [{
             mimetype: 'image/svg+xml', scheme: 'data', type: 'asset/resource', generator: {
